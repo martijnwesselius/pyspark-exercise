a2d392edd9d257ceaddce1d99fd29503f93f5bb0
Martijn Wesselius <63151285+martijnwesselius@users.noreply.github.com>
2023-09-07 15:16:36 +0200

    Add function type checking

--- join.py
+++ join.py
@@ -1,10 +1,19 @@
 import os
 import argparse
-from pyspark.sql import SparkSession
+from typing import Dict, List
+
+import pyspark
+from pyspark.sql import SparkSession, DataFrame, Column
+# from chispa.column_comparer import assert_column_equality
 
 
 """
 Module Docstring
+
+C:/Users/Martijn/miniconda3/python.exe 
+"c:/Users/Martijn/Desktop/PySpark Exercise ABN Amro/join.py" 
+--fpath_1 'data/dataset_one.csv' --fpath_2 'data/dataset_two.csv' --countries 'United Kingdom' 'Netherlands'
+
 """
 
 __author__ = "Martijn Wesselius"
@@ -30,13 +39,13 @@ def parser():
     return args
     
 
-def filter(df, col_object, values):
+def filter(df: DataFrame, col_object: Column, values: List) -> DataFrame:
     """ ... """
 
     return df.filter(col_object.isin(values))
 
 
-def rename(df, col_names):
+def rename(df: DataFrame, col_names: Dict) -> DataFrame:
     """ ... """
 
     for col in col_names.keys():
@@ -45,7 +54,7 @@ def rename(df, col_names):
     return df
 
 
-def write_output(df):
+def write_output(df: DataFrame):
     """ ... """  
 
     # df.write.mode('overwrite').format('csv').options(header='True', delimiter=',').csv("client_data/result.csv")
@@ -68,27 +77,35 @@ def main():
     # check arguments
 
     # Create Spark session
+    # master('local').appname('chispa').
     spark = SparkSession.builder.getOrCreate()
 
     # Create Spark dataframes from csv
     df1 = spark.read.csv(args.fpath_1, header=True)
-    df1 = df1.select('id', 'email', 'country')
+    df1 = df1.select(['id', 'email', 'country'])
 
     df2 = spark.read.csv(args.fpath_2, header=True)
-    df2 = df2.select('id', 'btc_a', 'cc_t')
+    df2 = df2.select(['id', 'btc_a', 'cc_t'])
     
+    print(type(df1))
+    print(type(df1.id))
+    print(type(args))
+
     # Join dataframes
+    # assert_df_equality(df1, df2, ignore_row_order=True)
     df = df1.join(df2, on='id', how='leftouter')
 
     # Rename columns
-    col_names = {"id":"client_identifier", "btc_a":"bitcoin_address", "cc_t":"credit_card_type"}
+    col_names = {"id":"client_identifier", 
+                 "btc_a":"bitcoin_address", 
+                 "cc_t":"credit_card_type"}
     df = rename(df, col_names)
     
     # Filter countries
     df = filter(df, df.country, args.countries)
 
     # Write dataframe to csv
-    write_output(df)
+    # write_output(df)
 
 
     df.show()
